b70242a637c58513dab63695841952ccbe1ab2bc
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-04-04 10:38:09 -0300

    implementation to second day

--- src/main/java/br/com/devcanoa/imdbapi/ImdbApiApplication.java
+++ src/main/java/br/com/devcanoa/imdbapi/ImdbApiApplication.java
@@ -2,11 +2,16 @@ package br.com.devcanoa.imdbapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.json.JacksonJsonParser;
 
+import javax.management.Attribute;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.*;
+import java.util.jar.Attributes;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class ImdbApiApplication {
@@ -17,12 +22,31 @@ public class ImdbApiApplication {
 
         String apiKey = System.getenv("apiKey");
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://imdb-api.com/en/API/Search/" + apiKey + "/inception%202010"))
+                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/" + apiKey))
                 .GET()
                 .build();
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
+        String response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body).join();
+
+        List<Object> moviesList = getMoviesList(response);
+
+        List<String> titles = getListAttributes("title", moviesList);
+        List<String> urlImages = getListAttributes("image", moviesList);
+        List<String> rating = getListAttributes("imDbRating", moviesList);
+        List<String> years = getListAttributes("year", moviesList);
+
+//        System.out.println(objects);
+    }
+
+    private static List<String> getListAttributes(String type, List<Object> moviesList) {
+        List<String> strings = moviesList.stream().map(movie -> movie.toString().split(type+"=")[1].split(",")[0]).collect(Collectors.toList());
+        return strings;
+    }
+
+    private static List<Object> getMoviesList(String response) {
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        Map<String, Object> objects = jsonParser.parseMap(response);
+        List<Object> moviesList = (ArrayList) objects.get("items");
+        return moviesList;
     }
 }
